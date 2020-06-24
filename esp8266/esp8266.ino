6a41327cc225791a917983d249aa4024230832d8
Orel <aurelien.chirot@idee-lab.fr>
2020-06-24 21:44:15 +0200

    bear ssl + power consumption improvement

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -20,38 +20,54 @@ https://byfeel.info/eeprom-ou-spiffs/
 // HTTP server
 #include <ESP8266WiFi.h>
 
+// For BEAR SSL
+#include <ESP8266HTTPClient.h>
+
 // HTTPS client
 #include <WiFiClientSecure.h>
 
 // HTTP client
 #include <HttpClient.h>
 
-
 #include "config.h"
 
+// to get getVcc() to work, has the doc say :
+// http://arduino.esp8266.com/Arduino/versions/2.0.0-rc2/doc/libraries.html#esp-specific-apis
+ADC_MODE(ADC_VCC);
+
 // Infrared port
-const int IR_LED = 4;
+const int IR_LED = 4;   // D2
+const int IR_POWER = 5; // D1
 
 // interval between two checkings, in seconds
-const int interval = 60 * 10;
+//const int interval = 60 * 10;
+const int interval = 10;
 
 void setup()
 {
   Serial.begin(57600);
   SPIFFS.begin();
 
+  // The infrared module supply is wired in a PIN
+  // instead of the 3.3V, so it is OFF when the
+  // ESP12 is in deepsleep mode.
+  // So we have explicitly to turn on the PIN to
+  // give power to the infrared module.
+  pinMode(IR_POWER, OUTPUT);
+  digitalWrite(IR_POWER, HIGH);
+  delay(10);
+
   // prev & current states
   pinMode(IR_LED, INPUT_PULLUP);
   bool state = !digitalRead(IR_LED);
   bool prev_state = load_state();
 
-  
-  if(!prev_state && state) // You have new mail !
+  if (!prev_state && state) // You have new mail !
   {
     // Wifi
     wifi_connect();
 
-    if( https_free_smsapi(String("You have mail ! (batt:")+ESP.getVcc()+")") )
+    if (bear_https_free_smsapi(String("You have mail ! (batt:") + ESP.getVcc() + ")"))
     {
       save_state(state);
       Serial.println("You have mail !");
@@ -64,23 +80,20 @@ void setup()
   }
   else
   {
-    Serial.println(String("Mailbox is ") + (state?"full":"empty"));
+    Serial.println(String("Mailbox is ") + (state ? "full" : "empty"));
     // http_debug(String("Mailbox is ") + (state ? "full" : "empty"));
 
     save_state(state);
   }
 
-
   // Sleep
   ESP.deepSleep(interval * 1000000);
-  //delay(interval * 1000);  
+  //delay(interval * 1000);
 }
 
 void loop()
 {
 }
-
-
 
 void wifi_connect()
 {
@@ -91,16 +104,52 @@ void wifi_connect()
   {
     delay(500);
     Serial.print(".");
+    Serial.print(WiFi.status());
   }
 }
 
+bool bear_https_free_smsapi(String message)
+{
+  BearSSL::WiFiClientSecure client;
+  client.setInsecure();
+  HTTPClient https;
+
+  String url = String("/sendmsg?user=") + FREE_USER + "&pass=" + FREE_PASS + "&msg=" + message;
+
+  if (https.begin(client, "smsapi.free-mobile.fr", 443, url))
+  {
+    int httpsCode = https.GET();
+    if (httpsCode > 0)
+    {
+      Serial.println(httpsCode);
+      if (httpsCode == HTTP_CODE_OK)
+      {
+        Serial.println(https.getString());
+        return true;
+      }
+    }
+    else
+    {
+      Serial.print("failed to GET");
+      return false;
+    }
+  }
+  else
+  {
+    Serial.print("failed to connect to server");
+    return false;
+  }
+}
 
 
 bool https_free_smsapi(String message)
 {
   WiFiClientSecure client;
 
-  const char *fingerprint = "3B 5C 64 35 F5 28 BF 1C FA 96 DC E7 65 B1 E1 B3 2E 84 35 77";
+  // SHA-1 fingerprint
+  //const char *fingerprint = "3B 5C 64 35 F5 28 BF 1C FA 96 DC E7 65 B1 E1 B3 2E 84 35 77";
+  // 2020-06
+  const char *fingerprint = "EB 49 4D F9 6A 8A 87 FC 7D D0 CA 7E 99 20 15 47 37 FC AD 4E";
 
   client.setFingerprint(fingerprint);
 
@@ -124,8 +173,6 @@ bool https_free_smsapi(String message)
   return true;
 }
 
-
-
 void http_debug(String message)
 {
   WiFiClient client;
@@ -133,8 +180,6 @@ void http_debug(String message)
   client.println(String("GET /") + urlencode(message) + " HTTP/1.1\n\n");
   client.stop();
 }
-
-
 
 String urlencode(String str)
 {
@@ -178,26 +223,25 @@ String urlencode(String str)
   return encodedString;
 }
 
-
-
 void save_state(bool state)
 {
-  if ( ! SPIFFS.exists("/state.txt")) {
+  if (!SPIFFS.exists("/state.txt"))
+  {
     SPIFFS.format();
     Serial.println("state.txt  formatted");
   }
 
   File f = SPIFFS.open("/state.txt", "w");
-  f.print(state?"1":"0");
+  f.print(state ? "1" : "0");
   f.close();
 }
-
 
 bool load_state()
 {
   String value = "";
 
-  if ( ! SPIFFS.exists("/state.txt")) {
+  if (!SPIFFS.exists("/state.txt"))
+  {
     SPIFFS.format();
     Serial.println("state.txt  formatted");
   }
@@ -211,5 +255,5 @@ bool load_state()
 
   file.close();
 
-  return value=="1";
+  return value == "1";
 }
